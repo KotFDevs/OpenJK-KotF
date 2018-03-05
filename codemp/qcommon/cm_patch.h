23982d5d57a495d274b597315763f861f4eaf24b
Knights of the Force 2.1 <linken233@hotmail.com>
2018-03-05 17:04:37 -0500

    TatooineDuel should now be accessible.
    Players should now be able to access TatooineDuel.

--- codemp/qcommon/cm_patch.h
+++ codemp/qcommon/cm_patch.h
@@ -63,8 +63,8 @@ properly.
 */
 
 
-#define	MAX_FACETS			1024
-#define	MAX_PATCH_PLANES	2048
+#define	MAX_FACETS			4096
+#define	MAX_PATCH_PLANES	8192
 
 typedef struct patchPlane_s {
 	float	plane[4];
